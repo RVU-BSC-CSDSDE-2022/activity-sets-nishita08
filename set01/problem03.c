1984171471cb3787b74b26dcf3a3acd5b66a587c
nishita08 <119583511+nishita08@users.noreply.github.com>
2022-12-05 05:05:39 +0000

    solved

---
+++ set01/problem03.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (void)
+{
+  int a , b;
+  printf("Enter a");
+  scanf("%d",&a);
+
+  printf("Enter b");
+  scanf("%d",&b);
+  
+  printf("sum is : %d",a + b);
+  return 0;
+  }
+  
