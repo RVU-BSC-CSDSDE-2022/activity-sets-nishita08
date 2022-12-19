54559cf937e9310d5c7a21d6aba5686b3c2d1fbe
nishita08 <119583511+nishita08@users.noreply.github.com>
2022-12-19 10:35:04 +0000

    not solved

--- set05/problem04.c
+++ set05/problem04.c
@@ -5,3 +5,5 @@ void input_camel_details(float *radius, float *height, float *length);
 int find_mood(float radius, float height, float length)
 void output(float radius, float height, float length, int mood);
 
+
+
